2da99322f4823ee797bdd310f1f740fab56813ca
D1ckenS <93459193+D1ckenS@users.noreply.github.com>
2024-08-05 20:30:45 +0300

    nthng serious

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
  
-<<<<<<< HEAD
-export const experimenta_ppr = true;
-=======
 const experimental_ppr = true;
->>>>>>> 23483943df099408e5239dfcf880d4af810dbf08
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
