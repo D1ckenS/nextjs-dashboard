f6212aaf121f786799b71580dec005791afe3fdb
D1ckenS <93459193+D1ckenS@users.noreply.github.com>
2024-08-06 13:43:25 +0300

    creating a dynamic route for updating invoices, changing href for UpdateInvoice component, binding id with updateInvoice function, creating updateInvoice function in 'action.ts'

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,6 +21,7 @@ export async function createInvoice(formData: FormData) {
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -30,6 +31,30 @@ export async function createInvoice(formData: FormData) {
     VALUES
     (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql `
+    UPDATE invoices
+    SET
+    customer_id = ${customerId},
+    amount = ${amountInCents},
+    status = ${status}
+    WHERE id = ${id}
+    `;
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
