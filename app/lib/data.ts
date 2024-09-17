36c65364704637a6dc70ab28b91cb976213a762b
D1ckenS <93459193+D1ckenS@users.noreply.github.com>
2024-09-17 13:04:35 +0300

    changing 'const data' in data.ts to 'let data'

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -66,7 +66,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
-    const data = await Promise.all([
+    let data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
       invoiceStatusPromise,
